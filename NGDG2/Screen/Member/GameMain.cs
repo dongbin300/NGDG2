62394b3cb4e38d99cab5052df552f38d477e3051
김동빈 <dongbin300@gmail.com>
2020-03-05 12:41:11 +0900

    test

--- NGDG2/Screen/Member/GameMain.cs
+++ NGDG2/Screen/Member/GameMain.cs
@@ -35,7 +35,7 @@ namespace NGDG2
             switch (key)
             {
                 case ConsoleKey.A:
-                    //TODO: 계정 저장
+                    //TODO: 계정 저장1
                     break;
                 case ConsoleKey.S:
                     ScreenManager.CurrentScreen = ScreenManager.Screen.DungeonSelection;
